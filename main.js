11824c3e33b2c49592c06e095b84d04b2bea0aac
jonnathas luiz costa dos santos <jonnathas_jl@hotmail.com>
2024-04-04 09:46:33 -0300

    renomeando projeto front end

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function createWindow () {
     }
   })
 
-  win.loadURL(path.join(__dirname + '/pdv-crew', 'build', 'index.html'));
+  win.loadURL(path.join(__dirname + '/front-end', 'build', 'index.html'));
 }
 
 app.whenReady().then(() => {
